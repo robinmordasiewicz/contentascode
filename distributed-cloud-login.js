7b198462c0812653ccb53289c5e4650cb9194663
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-06-23 10:14:20 -0400

    updating intro

--- distributed-cloud-login.js
+++ distributed-cloud-login.js
@@ -171,30 +171,13 @@ if ( !PASSWORD ) {
           size: 500,
           delay: 250
         });
+        await autoScroll(targetPage);
     }
 
 
- await autoScroll(page);
 
    // window.scrollBy(0, document.body.scrollHeight);
 
-const autoScroll = async (page) => {
-  await page.evaluate(async () => {
-    await new Promise((resolve, reject) => {
-      let totalHeight = 0
-      let distance = 100
-      let timer = setInterval(() => {
-        let scrollHeight = document.body.scrollHeight
-        window.scrollBy(0, distance)
-        totalHeight += distance
-        if(totalHeight >= scrollHeight){
-          clearInterval(timer)
-          resolve()
-        }
-      }, 100)
-    })
-  })
-}
     await page.evaluate(() => { window.scrollBy(0,document.body.scrollHeight); });
 
 await page.evaluate( () => {
