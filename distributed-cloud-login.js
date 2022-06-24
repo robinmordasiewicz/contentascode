f893f79a531f3e64ab66ecac828ed49c0bb75fc4
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-06-24 16:05:23 -0400

    updating intro

--- distributed-cloud-login.js
+++ distributed-cloud-login.js
@@ -74,7 +74,168 @@ if ( !PASSWORD ) {
     console.log("Pause for 5 seconds");
     await page.waitForTimeout(5000);
 
-    const { autoScroll,waitForSelectors, scrollIntoViewIfNeeded, waitForConnected, waitForInViewport, waitForSelector, waitForElement, querySelectorsAll, querySelectorAll, waitForFunction } = require("./puppeteer-functions.mjs");
+//    const { autoScroll,waitForSelectors, scrollIntoViewIfNeeded, waitForConnected, waitForInViewport, waitForSelector, waitForElement, querySelectorsAll, querySelectorAll, waitForFunction } = require("./puppeteer-functions.mjs");
+    async function waitForSelectors(selectors, frame, options) {
+      for (const selector of selectors) {
+        try {
+          return await waitForSelector(selector, frame, options);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      throw new Error('Could not find element for selectors: ' + JSON.stringify(selectors));
+    }
+
+    async function scrollIntoViewIfNeeded(element, timeout) {
+      await waitForConnected(element, timeout);
+      const isInViewport = await element.isIntersectingViewport({threshold: 0});
+      if (isInViewport) {
+        return;
+      }
+      await element.evaluate(element => {
+        element.scrollIntoView({
+          block: 'center',
+          inline: 'center',
+          behavior: 'smooth',
+        });
+      });
+      await waitForInViewport(element, timeout);
+    }
+
+    async function waitForConnected(element, timeout) {
+      await waitForFunction(async () => {
+        return await element.getProperty('isConnected');
+      }, timeout);
+    }
+
+    async function waitForInViewport(element, timeout) {
+      await waitForFunction(async () => {
+        return await element.isIntersectingViewport({threshold: 0});
+      }, timeout);
+    }
+
+    async function waitForSelector(selector, frame, options) {
+      if (!Array.isArray(selector)) {
+        selector = [selector];
+      }
+      if (!selector.length) {
+        throw new Error('Empty selector provided to waitForSelector');
+      }
+      let element = null;
+      for (let i = 0; i < selector.length; i++) {
+        const part = selector[i];
+        if (element) {
+          element = await element.waitForSelector(part, options);
+        } else {
+          element = await frame.waitForSelector(part, options);
+        }
+        if (!element) {
+          throw new Error('Could not find element: ' + selector.join('>>'));
+        }
+        if (i < selector.length - 1) {
+          element = (await element.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement();
+        }
+      }
+      if (!element) {
+        throw new Error('Could not find element: ' + selector.join('|'));
+      }
+      return element;
+    }
+
+    async function waitForElement(step, frame, timeout) {
+      const count = step.count || 1;
+      const operator = step.operator || '>=';
+      const comp = {
+        '==': (a, b) => a === b,
+        '>=': (a, b) => a >= b,
+        '<=': (a, b) => a <= b,
+      };
+      const compFn = comp[operator];
+      await waitForFunction(async () => {
+        const elements = await querySelectorsAll(step.selectors, frame);
+        return compFn(elements.length, count);
+      }, timeout);
+    }
+
+    async function querySelectorsAll(selectors, frame) {
+      for (const selector of selectors) {
+        const result = await querySelectorAll(selector, frame);
+        if (result.length) {
+          return result;
+        }
+      }
+      return [];
+    }
+
+    async function querySelectorAll(selector, frame) {
+      if (!Array.isArray(selector)) {
+        selector = [selector];
+      }
+      if (!selector.length) {
+        throw new Error('Empty selector provided to querySelectorAll');
+      }
+      let elements = [];
+      for (let i = 0; i < selector.length; i++) {
+        const part = selector[i];
+        if (i === 0) {
+          elements = await frame.$$(part);
+        } else {
+          const tmpElements = elements;
+          elements = [];
+          for (const el of tmpElements) {
+            elements.push(...(await el.$$(part)));
+          }
+        }
+        if (elements.length === 0) {
+          return [];
+        }
+        if (i < selector.length - 1) {
+          const tmpElements = [];
+          for (const el of elements) {
+            const newEl = (await el.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement();
+            if (newEl) {
+              tmpElements.push(newEl);
+            }
+          }
+          elements = tmpElements;
+        }
+      }
+      return elements;
+    }
+
+    async function waitForFunction(fn, timeout) {
+      let isActive = true;
+      setTimeout(() => {
+        isActive = false;
+      }, timeout);
+      while (isActive) {
+        const result = await fn();
+        if (result) {
+          return;
+        }
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+      throw new Error('Timed out');
+    }
+
+    async function autoScroll(page){
+        await page.evaluate(async () => {
+            await new Promise((resolve, reject) => {
+                var totalHeight = 0;
+                var distance = 100;
+                var timer = setInterval(() => {
+                    var scrollHeight = document.body.scrollHeight;
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
+
+                    if(totalHeight >= scrollHeight){
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 100);
+            });
+        });
+    }
 
     {
         console.log("Set Viewport to 936p");
